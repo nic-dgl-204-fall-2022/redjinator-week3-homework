be44bc9a085f3fcc774627052aae846940f98c52
Redjinator <rmcpherson@northislandcollege.ca>
2022-10-04 21:10:13 -0700

    Homework complete

--- src/main/kotlin/Interfaces.kt
+++ src/main/kotlin/Interfaces.kt
@@ -1,17 +1,3 @@
-/*
-Course: DGL 204 - Programming for mobile app development
-Author: Ashley Blacquiere
-
-Week 3 content, including interfaces
-*/
-
-
-abstract class ServiceStation(
-    val priceOfGasPerLitre: Double = 2.3
-) {
-
-}
-
 interface Pumpable {
     fun pumpGas(fundsProvided: Double)
 }
@@ -20,6 +6,30 @@ interface Payable {
     fun acceptPayment(fundsProvided: Double)
 }
 
+interface Fillable {
+    fun fillTank(station: ShellStation)
+}
+
+interface Drivable {
+    val topSpeed: Double
+    fun accelerate()
+    fun brake()
+}
+
+interface TimeTravelable {
+    var charged: Boolean
+    fun chargeFluxCapacitor()
+    fun timeTravel()
+}
+
+abstract class ServiceStation(
+    val priceOfGasPerLitre: Double = 2.3
+    ) {
+
+    }
+
+
+// Shell Station
 class ShellStation(
     val name: String = "Shell Station"
 ) : ServiceStation(), Pumpable, Payable {
@@ -29,32 +39,81 @@ class ShellStation(
     }
 
     override fun acceptPayment(fundsProvided: Double) {
-        TODO("Not yet implemented")
+        println("Accepted payment of $fundsProvided")
     }
 }
 
+// Vehicle
 open class Vehicle(
     val cashAvailable: Double = 10.0
-) {
-    fun fillTank(station: ShellStation) {
-        station.pumpGas(cashAvailable)
-    }
-}
+    ) { }
 
-class Car(
+
+
+open class Car(
 
 ) : Vehicle(
 
-) {
+), Fillable, Drivable {
 
+    override val topSpeed: Double = 100.0
+
+    override fun fillTank(station: ShellStation) {
+        station.acceptPayment(cashAvailable)
+        station.pumpGas(cashAvailable)
+    }
+
+    override fun accelerate() {
+        println("Accelerating...")
+    }
+
+    override fun brake() {
+        println("Braking...")
+    }
 }
 
+
+
+class Dalorian(
+
+): Car(), TimeTravelable {
+
+    override var charged: Boolean = false
+
+    override fun chargeFluxCapacitor() {
+        if (charged == false) {
+            charged = true
+            println("You refill the Flux Capacitor with plutonium and set the meter to 1.21 gigawatts, a few moments later it is fully charged")
+        } else {
+            println("Flux Capacitor is already fully charged")
+        }
+    }
+
+    override fun timeTravel() {
+        if (charged == false) {
+            println("You attempt to time travel but quickly realize the Flux Capacitor is not charged")
+        } else {
+            accelerate()
+            println("You reach 88mph and travel back in time")
+        }
+    }
+}
 
 fun main() {
+    val shellStation = ShellStation()
+    val car = Car()
+    car.fillTank(shellStation)
 
-    val s = ShellStation()
-    val c = Car()
+    // Create Dalorian inherits from Car
+    val d = Dalorian()
 
-    c.fillTank(s)
+    // Attempt to time travel without charging the Flux Capacitor
+    d.timeTravel()
 
+    // Charge the Flux Capacitor
+    d.chargeFluxCapacitor()
+
+    // Time travel
+    d.timeTravel()
 }
+
